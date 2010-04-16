201bb49f84c06f76c83ab82115f62561f30b967f
joan.navarrete.sanchez <joan.navarrete.sanchez@gmail.com>
2010-04-16 22:29:38 +0000

    Daos Genericos, Hibernate Templates de Spring y Busquedas con Hibernate Search

--- OPFind/src/main/java/es/opfind/dao/Dao.java
+++ OPFind/src/main/java/es/opfind/dao/Dao.java
@@ -1,13 +1,28 @@
 package es.opfind.dao;
 
+import java.io.Serializable;
 import java.util.List;
+
+import org.apache.lucene.search.Sort;
+import org.hibernate.criterion.DetachedCriteria;
 
 public interface Dao {
 
 	public <T> List<T> findByFullText(Class<T> entityClass, String[] entityFields, String textToFind);
+	
+	public <T> List<T> findByFullTextAndSort(Class<T> entityClass, String[] entityFields, String textToFind,Sort sort);
 
 	public void persist(Object entity);
 
 	public void persist(Object[] entities);
 
+	public <T> List<T> find(Class<T> entityClass);
+
+	public <T> T get(Class<T> entityClass, Serializable id);
+	
+	public <T> T load(Class<T> entityClass, Serializable id);
+
+	public <T> List<T> find(String hql);
+	
+	public <T> List<T> findByCriteria(DetachedCriteria criteria);
 }
--- OPFind/src/main/java/es/opfind/dao/GenericDao.java
+++ OPFind/src/main/java/es/opfind/dao/GenericDao.java
@@ -1,5 +1,6 @@
 package es.opfind.dao;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +9,10 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.queryParser.MultiFieldQueryParser;
 import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.search.Sort;
 import org.hibernate.Query;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.search.FullTextSession;
 import org.hibernate.search.Search;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class GenericDao extends HibernateDaoSupport implements Dao {
 
 	private static final Log log = LogFactory.getLog(GenericDao.class);
-	
+
 	@Autowired
 	public GenericDao(SessionFactory sessionFactory) {
 		super.setSessionFactory(sessionFactory);
@@ -42,16 +45,65 @@ public class GenericDao extends HibernateDaoSupport implements Dao {
 		final Query query = fullTextSession.createFullTextQuery(luceneQuery, entityClass);
 		return query.list();
 	}
+	
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly = true)
+	public <T> List<T> findByFullTextAndSort(Class<T> entityClass, String[] entityFields, String textToFind,Sort sort) {
+		final FullTextSession fullTextSession = Search.createFullTextSession(getSession());
+		final MultiFieldQueryParser parser = new MultiFieldQueryParser(entityFields, new StandardAnalyzer());
+		final org.apache.lucene.search.Query luceneQuery;
+		try {
+			luceneQuery = parser.parse(textToFind);
+		} catch (ParseException e) {
+			log.error("Cannot parse [" + textToFind + "] to a full text query", e);
+			return new ArrayList<T>(0);
+		}
+		final Query query = fullTextSession.createFullTextQuery(luceneQuery, entityClass).setSort(sort);
+		return query.list();
+	}
+
 
 	@Transactional
 	public void persist(Object entity) {
-		getHibernateTemplate().persist(entity);
+		  getHibernateTemplate().saveOrUpdate(entity);
 	}
 
 	@Transactional
 	public void persist(Object[] entities) {
-		for (Object entity: entities) {
+		for (Object entity : entities) {
 			persist(entity);
 		}
 	}
+
+	@Transactional(readOnly = true)
+	public <T> List<T> find(Class<T> entityClass) {
+		final List<T> entities = getHibernateTemplate().loadAll(entityClass);
+		return entities;
+	}
+
+	@Transactional(readOnly = true)
+	public <T> T load(Class<T> entityClass, Serializable id) {
+		final T entity = (T) getHibernateTemplate().load(entityClass, id);
+		return entity;
+	}
+	
+	@Transactional(readOnly = true)
+	public <T> T get(Class<T> entityClass, Serializable id) {
+		final T entity = (T) getHibernateTemplate().get(entityClass, id);
+		return entity;
+	}
+
+	@Transactional(readOnly = true)
+	public <T> List<T> find(String hql) {
+		final List<T> entities = getHibernateTemplate().find(hql);
+		return entities;
+	}
+
+	@Transactional(readOnly = true)
+	public <T> List<T> findByCriteria(DetachedCriteria criteria) {
+		final List<T> entities = getHibernateTemplate().findByCriteria(criteria);
+		return entities;
+	}
+	
+	
 }
