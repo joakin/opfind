dafac5f6380733cb07b2db5ad52f6aee724f445e
Joaquin <joakin@users.noreply.github.com>
2010-04-17 14:38:25 +0000

    Anyadido highlight de palabras al buscar!

--- OPFind/src/main/webapp/js/custom.js
+++ OPFind/src/main/webapp/js/custom.js
@@ -1,4 +1,15 @@
-$(document).ready(function() {
-	var texto=$("input.textinput").val();
-	
- });
+ $(document).ready(function() {
+    var texto=$(".searchterms").html();
+    if(texto && texto!="")
+    {
+        texto = texto.replace(/\"/ig, "");
+        alert(texto);
+        var words = texto.split(" AND ");
+        var i=0;
+        for(i=0;i<words.length;i++)
+        {
+            alert(words[i]);
+            $(".searchtext").highlight(words[i]);
+        }
+    }
+});
