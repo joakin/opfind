034dfe5ac5ab65cd75fea57a5dca1970d13f404c
Joaquin <joakin@users.noreply.github.com>
2010-04-17 14:41:55 +0000

    Anyadido highlight de palabras al buscar!

--- OPFind/src/main/webapp/js/custom.js
+++ OPFind/src/main/webapp/js/custom.js
@@ -3,12 +3,10 @@
     if(texto && texto!="")
     {
         texto = texto.replace(/\"/ig, "");
-        alert(texto);
         var words = texto.split(" AND ");
         var i=0;
         for(i=0;i<words.length;i++)
         {
-            alert(words[i]);
             $(".searchtext").highlight(words[i]);
         }
     }
