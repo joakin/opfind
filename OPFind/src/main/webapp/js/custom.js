619c3b6aaffa416a6348316bef30388cad80dacc
Joaquin <joakin@users.noreply.github.com>
2010-04-17 17:40:29 +0000

    Javascripteado el modulito de Ultimas noticias del index

--- OPFind/src/main/webapp/js/custom.js
+++ OPFind/src/main/webapp/js/custom.js
@@ -10,4 +10,23 @@
             $(".searchtext").highlight(words[i]);
         }
     }
+
+    if($("#ultconv").length)
+    {
+        $(".smallitem>span").children("div").hide();
+        for(x=0;x<4;x++)$(".smallitem>span").children("div:not(.shown)").not(":visible").last().slideDown("slow").addClass("shown");
+        timer=setInterval(showItem, 6000);
+    }
+
 });
+
+function showItem()
+{
+    var list=$(".smallitem>span").children("div:not(.shown)").not(":visible");
+    if(list.length==0)
+        clearInterval(timer);
+    else{
+        list.last().slideDown("slow").addClass("shown");
+        $(".smallitem>span").children("div.shown:visible").last().slideUp("slow");
+    }
+}
