5bd07ec4a3ffc3adb0fa7e5359a4a026c6fd77df
joan.navarrete.sanchez <joan.navarrete.sanchez@gmail.com>
2010-04-17 11:33:09 +0000

    joaquin no tiene 1.5 se modifica isempty

--- OPFind/src/main/java/es/opfind/service/CivilJobMgr.java
+++ OPFind/src/main/java/es/opfind/service/CivilJobMgr.java
@@ -1,6 +1,6 @@
 package es.opfind.service;
 
-import java.util.Date;  
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -12,8 +12,11 @@ import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Service;
 
+import com.ibm.icu.util.Calendar;
+
 import es.opfind.dao.Dao;
 import es.opfind.domain.CivilJob;
+import es.opfind.util.DateUtils;
 
 @Service
 public class CivilJobMgr {
@@ -21,7 +24,7 @@ public class CivilJobMgr {
 	@Resource
 	private Dao dao;
 
-	public CivilJob newCivilJob(String description, String url, String fullHtml, String category,String num) {
+	public CivilJob newCivilJob(String description, String url, String fullHtml, String category, String num) {
 		CivilJob job = new CivilJob();
 		job.setDescription(description);
 		job.setUrl(url);
@@ -32,16 +35,18 @@ public class CivilJobMgr {
 	}
 
 	public void persist(CivilJob civilJob) {
-		//TODO: En los tres Mgrs esta esto, hibernate no funciona con los campos automaticos, quitarlo
+		// TODO: En los tres Mgrs esta esto, hibernate no funciona con los
+		// campos automaticos, quitarlo
 		if (civilJob.getCreated() == null)
 			civilJob.setCreated(new Date());
 		civilJob.setModified(new Date());
 		dao.persist(civilJob);
 	}
 
-	public CivilJob load(Integer id){
+	public CivilJob load(Integer id) {
 		return dao.load(CivilJob.class, id);
 	}
+
 	public void persist(CivilJob civilJob[]) {
 		dao.persist(civilJob);
 	}
@@ -52,30 +57,74 @@ public class CivilJobMgr {
 	}
 
 	public List<CivilJob> findByFullText(String textToFind) {
-		final List<CivilJob> list = dao.findByFullText(CivilJob.class, CivilJob.FULL_TEXT_FIELDS,textToFind);
+		final List<CivilJob> list = dao.findByFullText(CivilJob.class, CivilJob.FULL_TEXT_FIELDS, textToFind);
 		return list;
 	}
-	
+
 	public List<CivilJob> findByFullTextAndSortDate(String textToFind) {
-		Sort sort = new Sort( new SortField( "bolDate", SortField.AUTO, true ) );
-		final List<CivilJob> list = dao.findByFullTextAndSort(CivilJob.class, CivilJob.FULL_TEXT_FIELDS,textToFind,sort);
+		Sort sort = new Sort(new SortField("bolDate", SortField.AUTO, true));
+		final List<CivilJob> list = dao.findByFullTextAndSort(CivilJob.class, CivilJob.FULL_TEXT_FIELDS, textToFind,
+				sort);
 		return list;
 	}
-	
+
 	public List<CivilJob> getCivilJobOrderByDate(String name) {
 		DetachedCriteria criteria = DetachedCriteria.forClass(CivilJob.class);
 		criteria.addOrder(Order.desc("newsletter.bolDate"));
 		final List<CivilJob> list = dao.findByCriteria(criteria);
 		return list;
 	}
-	
-	public CivilJob getCivilJobByNum(String num){
+
+	public CivilJob getCivilJobByNum(String num) {
 		DetachedCriteria criteria = DetachedCriteria.forClass(CivilJob.class);
-		 criteria.add(Restrictions.eq("num", num));
-		 final List<CivilJob> list = dao.findByCriteria(criteria);
-		 if ( !list.isEmpty() )
-			 return list.get(0);
-		 else
-			 return null; 
+		criteria.add(Restrictions.eq("num", num));
+		final List<CivilJob> list = dao.findByCriteria(criteria);
+		if (!list.equals(""))
+			return list.get(0);
+		else
+			return null;
+	}
+
+	public List<CivilJob> getUpdatesByDateAndSearch(Date lastSendDate, String search) {
+		List<CivilJob> civilJobs = findByFullTextAndSortDate(search);
+
+		if ( civilJobs.equals(""))
+			return civilJobs;
+		
+		Integer index = binaryDateSearch(lastSendDate, civilJobs);
+		
+		//Sigo recorriendo el array por si el binarysearch a caido alejado
+		int i = 0;
+		
+		for (i = index + 1; i < civilJobs.size(); i++) {
+			if (!DateUtils.setTimeToMidnight(civilJobs.get(i).getBolDate()).equals(
+					DateUtils.setTimeToMidnight(lastSendDate)))
+				break;
+		}
+		
+		return civilJobs.subList(0, i-1);
+	}
+
+	private Integer binaryDateSearch(Date lastSendDate, List<CivilJob> civilJobs) {
+
+		Integer min = 0;
+		Integer max = civilJobs.size() - 1;
+		Integer mid = 0;
+		do {
+			mid = (min + max) / 2;
+			if (DateUtils.setTimeToMidnight(civilJobs.get(mid).getBolDate()).before(
+					DateUtils.setTimeToMidnight(lastSendDate)))
+				max = mid - 1;
+			else
+				min = mid + 1;
+
+		} while (!DateUtils.setTimeToMidnight(civilJobs.get(mid).getBolDate()).equals(
+				DateUtils.setTimeToMidnight(lastSendDate))
+				&& min < max);
+
+		if (min >= max)
+			mid = -1;
+
+		return mid;
 	}
 }
--- OPFind/src/main/java/es/opfind/service/OPNewsletterMgr.java
+++ OPFind/src/main/java/es/opfind/service/OPNewsletterMgr.java
@@ -10,6 +10,8 @@ import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Service;
 
+import com.eaio.uuid.UUID;
+
 import es.opfind.dao.Dao;
 import es.opfind.domain.OPNewsletter;
 
@@ -19,27 +21,46 @@ public class OPNewsletterMgr implements Serializable {
 	@Resource
 	private Dao dao;
 
-	public OPNewsletter newOpNewsletter(String email,Boolean search,String searchText){
+	public OPNewsletter newOpNewsletter(String email,String searchText){
 		OPNewsletter opNewsletter = new OPNewsletter();
 		opNewsletter.setEmail(email);
-		opNewsletter.setSearch(search);
 		opNewsletter.setSearchText(searchText);
 		opNewsletter.setLastSendDate(new Date());
+		opNewsletter.setUuid(new UUID().toString());
 		
 		return opNewsletter;
 	}
 	
-	public OPNewsletter getOPNewsletterByEmail(String email){
+	public List<OPNewsletter> getOPNewslettersByEmail(String email){
 		DetachedCriteria criteria = DetachedCriteria.forClass(OPNewsletter.class);
 		 criteria.add(Restrictions.eq("email", email));
 		 final List<OPNewsletter> list = dao.findByCriteria(criteria);
-		 if (!list.isEmpty())
+		 return list;
+	}
+	
+	public List<OPNewsletter> getOPNewslettersByEmailAndSearchText(String email,String searchText){
+		DetachedCriteria criteria = DetachedCriteria.forClass(OPNewsletter.class);
+		 criteria.add(Restrictions.eq("email", email));
+		 criteria.add(Restrictions.eq("searchText", searchText));
+		 final List<OPNewsletter> list = dao.findByCriteria(criteria);
+		 return list;
+	}
+	
+	public OPNewsletter getOPNewsletterByUuid(String uuid){
+		DetachedCriteria criteria = DetachedCriteria.forClass(OPNewsletter.class);
+		 criteria.add(Restrictions.eq("uuid", uuid));
+		 final List<OPNewsletter> list = dao.findByCriteria(criteria);
+		 if(!list.equals(""))
 			 return list.get(0);
 		 return null;
 	}
 	
 	public void persist(OPNewsletter opNewsletter){
 		dao.persist(opNewsletter);
+	}
+	
+	public void delete(OPNewsletter opNewsletter){
+		dao.delete(opNewsletter);
 	}
 	
 }
